a2de8d2a34de8884a452cb15d997ea5473d9bda3
Black-Eagle-1 <blackeagle9487@gmail.com>
2024-03-21 11:09:24 +0800

    catcute

--- js/scripts.js
+++ js/scripts.js
@@ -53,3 +53,8 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+function playSound() {
+  var sound = document.getElementById("clickSound");
+  sound.play();
+}
