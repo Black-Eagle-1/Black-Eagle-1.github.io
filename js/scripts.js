d8890d62587b49b84a0d994f6a2e94929a612e9e
Black-Eagle-1 <blackeagle9487@gmail.com>
2024-03-27 16:45:33 +0800

    skills2

--- js/scripts.js
+++ js/scripts.js
@@ -48,13 +48,12 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+function playSound() {
+  var sound = document.getElementById("clickSound");
+  sound.play();
 }
 
 function playSound() {
-  var sound = document.getElementById("clickSound");
+  var sound = document.getElementById("catsound");
   sound.play();
 }
